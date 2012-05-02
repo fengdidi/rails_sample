1051bfc280f2932271033fd3ae9ee8cb833f979e
Di Feng <fengdidi@gmail.com>
2012-05-02 23:22:50 +0200

    finish chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Return title in per pages basis.
+  def title
+    bs = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      bs
+    else
+      "#{bs} | #{title}"
+    end
+  end
+  
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
     
     it "shoud have the right title" do
       get 'home'
-      response.shoud have_selector("title",
+      response.should have_selector("title",
                                     :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
